8506f0c4f6c95101344451631cf979972697baaa
Herbert Balagtas <hbalagtas@gmail.com>
2016-04-27 16:15:22 -0400

    Move authorize function to StravaProfileController

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,67 +21,16 @@ Route::group(['middleware' => ['auth','web']], function () {
 	Route::resource('biketype', 'BikeTypeController');
 	Route::resource('bikes', 'BikeController');
 
-	Route::get('select-components/{bike_id}', function($bike_id){
-		#echo $bike_id;
+	Route::get('select-components/{bike_id}', function($bike_id){		
 		$bike = Bike::findOrFail($bike_id);
 		$componenttypes = ComponentType::all();
 		return view('select-components', compact('bike', 'componenttypes'));
 	});
+	Route::post('select-components', function () {
+
+	});
 });
 
+Route::get('/home', ['as' => 'home', 'uses' => 'HomeController@index']);
 
-Route::get('/home', 'HomeController@index');
-
-Route::get('authorize', function(){
-	$code = Input::get('code');
-	$clientId = env('STRAVA_CLIENT_ID');
-	$clientSecret = env('STRAVA_CLIENT_SECRET');
-	
-	$api = \Cache::get('stravaapi', new StravaApi(
-	    $clientId,
-	    $clientSecret
-	));
-	$response = $api->tokenExchange($code);
-	$accessToken = $response->access_token;
-	$athlete = $response->athlete;
-
-	$user = User::firstOrCreate([
-		'name' => $athlete->firstname . ' ' . $athlete->lastname,
-		'email' => $athlete->email
-		
-		]);	
-	
-	if ($athlete->measurement_preference == 'meters' ){
-		$user->unit = 'km';
-	} else {
-		$user->unit = 'mi';
-	}
-	$user->save();
-
-	if ( is_null( $user->stravaprofile ) ){
-		$strava_profile = new BikeUsageTracker\StravaProfile;
-		$strava_profile->strava_id = $athlete->id;
-		$strava_profile->access_token = $accessToken;
-		$strava_profile->strava_data = serialize($response->athlete);
-		$strava_profile->save();
-		$user->stravaprofile()->save($strava_profile);		
-		$strava_profile->importbikes();
-	}	
-
-	Auth::login($user);	
-	return view('home');
-});
-
-/*Route::get('strava', function(){
-	\Cache::forget('stravaapi');
-	$clientId = env('STRAVA_CLIENT_ID');
-	$clientSecret = env('STRAVA_CLIENT_SECRET');
-	$api = new StravaApi(
-	    $clientId,
-	    $clientSecret
-	);
-	\Cache::put('stravaapi', $api, 50000);
-	$redirect = 'http://strava.app/authorize';
-	$url = $api->authenticationUrl($redirect, $approvalPrompt = 'auto', $scope = null, $state = null);
-	echo "<a href='{$url}'>Log in using Strava</a>";
-});*/
+Route::get('authorize', ['as' =>'authorize_strava', 'uses' => 'StravaProfileController@authorizestrava']);
